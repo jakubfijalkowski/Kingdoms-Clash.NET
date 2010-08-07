d41976bfc95f18386f527d4c67c5d2bcf1e48193
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-08 01:32:01 +0200

    Dodana struktura projektu, gitignore i biblioteki.

---
+++ Src/ClashEngine.NET.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClashEngine.NET.Tests")]
+[assembly: AssemblyDescription("Testy jednostkowe dla silnika ClashEngine.NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClashEngine.NET.Tests")]
+[assembly: AssemblyCopyright("Copyright © Jakub 'Fiołek' Fijałkowski 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0c64476d-1204-4da0-8c91-5d52024d57e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
---
+++ Src/Kingdoms Clash.NET/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kingdoms_Clash.NET
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
