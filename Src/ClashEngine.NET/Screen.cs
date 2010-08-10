fe3f1d8573e2d84b6e6ab63334a22dae5ebac16b
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-10 22:09:09 +0200

    Dodano szkielet managera ekranów.

---
+++ Src/ClashEngine.NET/Screen.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ClashEngine.NET
+{
+	/// <summary>
+	/// Stan ekranu.
+	/// </summary>
+	public enum ScreenState
+	{
+		/// <summary>
+		/// Aktywny.
+		/// </summary>
+		Active,
+
+		/// <summary>
+		/// Nieaktywny(np. zasłonięty przez inny nie-popup.
+		/// </summary>
+		Inactive,
+
+		/// <summary>
+		/// Zamknięty(nieodrysowywany ani nieuaktualniany, ale pozostający w managerze).
+		/// </summary>
+		Closed
+	}
+
+	/// <summary>
+	/// Bazowa klasa dla "ekranów"(np. menu, plansza).
+	/// Tylko jeden ekran może być aktywny, tzn. jeśli zostanie zasłonięty innym nie będzie aktualizowany.
+	/// To zachowanie można zmienić ustawiając właściwość IsPopup na true, wtedy ekran "pod" nadal będzie aktualizowany.
+	/// Jeśli ekran ma ustawioną właściwość IsFullscreen na true ekrany pod nie są odrysowywane - względy wydajnościowe.
+	/// </summary>
+	public abstract class Screen
+	{
+		private bool _IsPopup = false;
+		private bool _IsFullscreen = false;
+
+		#region Properties
+		/// <summary>
+		/// Manager - rodzic.
+		/// </summary>
+		public ScreensManager Manager { get; internal set; }
+
+		/// <summary>
+		/// Czy ekran jest ekranem "wyskakującym", czyli nie zasłania reszty a co za tym idzie pozwala na ich aktualizację.
+		/// Nie może być true jeśli IsFullscreen == true.
+		/// </summary>
+		public bool IsPopup
+		{
+			get { return this._IsPopup; }
+			set
+			{
+				if (value && this.IsFullscreen)
+				{
+					throw new ArgumentException("IsFullscreen is true");
+				}
+				this._IsPopup = value;
+			}
+		}
+
+		/// <summary>
+		/// Czy ekran jest pełnoekranowy(zasłania wszystko co pod).
+		/// Nie może być true jeśli IsPopup == true;
+		/// </summary>
+		public bool IsFullscreen
+		{
+			get { return this._IsFullscreen; }
+			set
+			{
+				if (value && this.IsPopup)
+				{
+					throw new ArgumentException("IsPopup is true");
+				}
+				this._IsFullscreen = value;
+			}
+		}
+
+		/// <summary>
+		/// Aktualny stan ekranu.
+		/// </summary>
+		public ScreenState State { get; internal set; }
+		#endregion
+
+		#region Events
+		/// <summary>
+		/// Uaktualnienie.
+		/// </summary>
+		/// <param name="delta">Czas od ostatniego uaktualnienia.</param>
+		public virtual void Update(double delta)
+		{ }
+
+		/// <summary>
+		/// Ekran ma się odrysować.
+		/// </summary>
+		public virtual void Render()
+		{ }
+
+		/// <summary>
+		/// Zmienił się stan ekranu.
+		/// </summary>
+		public virtual void StateChanged()
+		{ }
+		#endregion
+	}
+}
---
+++ Src/ClashEngine.NET/ScreensManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ClashEngine.NET
+{
+	/// <summary>
+	/// Manager ekranów.
+	/// </summary>
+	public class ScreensManager
+	{
+		private List<Screen> _ScreensList = new List<Screen>();
+
+		#region Properties
+		/// <summary>
+		/// Lista ekranów w managerze.
+		/// Bardziej przypomina stos/kolejkę LIFO(ostatni ekran na liście jest pierwszym "w rzeczywistości").
+		/// </summary>
+		public ReadOnlyCollection<Screen> ScreensList
+		{
+			get { return this._ScreensList.AsReadOnly(); }
+		}
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// Dodaje ekran do listy.
+		/// </summary>
+		/// <param name="screen">Ekran do dodania.</param>
+		public void AddScreen(Screen screen)
+		{ }
+
+		/// <summary>
+		/// Usuwa ekran z managera.
+		/// </summary>
+		/// <param name="screen">Ekran do usunięcia.</param>
+		public void RemoveScreen(Screen screen)
+		{ }
+
+		/// <summary>
+		/// Przesówa ekran na wierzch.
+		/// </summary>
+		/// <param name="screen">Ekran.</param>
+		public void MoveToFront(Screen screen)
+		{ }
+
+		/// <summary>
+		/// Przesówa ekran we wskazane miejsce.
+		/// </summary>
+		/// <param name="screen">Ekran.</param>
+		/// <param name="position">Pozycja w liście(licząc od końca!).</param>
+		public void MoveTo(Screen screen, int position)
+		{ }
+
+		/// <summary>
+		/// Zmienia ekran wskazany ekran na aktywny.
+		/// </summary>
+		/// <param name="screen">Ekran.</param>
+		public void MakeActive(Screen screen)
+		{ }
+
+		/// <summary>
+		/// Zmienia ekran na nieaktywny.
+		/// </summary>
+		/// <param name="screen">Ekran.</param>
+		public void MakeInactive(Screen screen)
+		{ }
+
+		/// <summary>
+		/// Zamyka ekran.
+		/// </summary>
+		/// <param name="screen">Ekran.</param>
+		public void Close(Screen screen)
+		{ }
+		#endregion
+	}
+}
