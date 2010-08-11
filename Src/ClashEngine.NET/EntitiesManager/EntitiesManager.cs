0c1ae4e65d27e60751c138fff3140044e30d8a0c
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-11 16:22:11 +0200

    Dalsza część poprawy literówki. Dodanie managera encji.

---
+++ Src/ClashEngine.NET/EntitiesManager/EntitiesManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ClashEngine.NET.EntitiesManager
+{
+	public class EntitiesManager
+	{
+		#region Properties
+		private List<GameEntity> Entities_;
+
+		/// <summary>
+		/// Lista encji.
+		/// </summary>
+		public ReadOnlyCollection<GameEntity> Entities
+		{
+			get { return this.Entities_.AsReadOnly(); }
+		}
+		#endregion
+
+		#region Methods
+		public void AddEntity(GameEntity entity)
+		{ }
+
+		public void RemoveEntity(GameEntity entity)
+		{ }
+
+		public void Update(double delta)
+		{ }
+
+		public void Render()
+		{ }
+		#endregion
+	}
+}
