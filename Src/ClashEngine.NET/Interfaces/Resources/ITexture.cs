5d21225d29e48cb7707fb26eb6ed392fa7154497
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-18 23:41:05 +0200

    Dodanie interfejsów dla tekstur i atlasów tekstur.

---
+++ Src/ClashEngine.NET/Interfaces/Resources/ITexture.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace ClashEngine.NET.Interfaces.Resources
+{
+	/// <summary>
+	/// Bazowy interfejs dla tekstur.
+	/// </summary>
+	public interface ITexture
+		: ResourcesManager.IResource
+	{
+		/// <summary>
+		/// Pobiera identyfikator(OpenGL) tekstury.
+		/// </summary>
+		int GetID { get; }
+
+		/// <summary>
+		/// Pobiera koordynaty tekstury.
+		/// </summary>
+		RectangleF Coordinates { get; }
+
+		/// <summary>
+		/// Ustawia teksturę jako aktualnie używaną.
+		/// </summary>
+		void Bind();
+	}
+}
---
+++ Src/ClashEngine.NET/Interfaces/Resources/ITexturesAtlas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ClashEngine.NET.Interfaces.Resources
+{
+	/// <summary>
+	/// Interfejs dla atlasu tekstur.
+	/// </summary>
+	public interface ITexturesAtlas
+		: ResourcesManager.IResource
+	{
+		/// <summary>
+		/// Pobiera wszystkie identyfikatory tekstur jakie są w atlasie.
+		/// </summary>
+		ICollection<string> IDs { get; }
+
+		/// <summary>
+		/// Pobiera teksturę o wskazanum ID.
+		/// </summary>
+		/// <param name="id">Identyfikator tekstury.</param>
+		/// <returns>Tekstura.</returns>
+		ITexture Get(string id);
+	}
+}
