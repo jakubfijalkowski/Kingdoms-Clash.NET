3ae4b5ab29c72c32c09a0a36176de329fc5e0638
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-21 13:25:45 +0200

    Dodanie wstępnego interfejsu dla shaderów i managera shaderów.

---
+++ Src/ClashEngine.NET/Interfaces/ShadersManager/IShader.cs
@@ -0,0 +1,47 @@
+﻿using OpenTK;
+
+namespace ClashEngine.NET.Interfaces.ShadersManager
+{
+	/// <summary>
+	/// Typ shadera.
+	/// </summary>
+	public enum ShaderType
+	{
+		/// <summary>
+		/// Pixel(fragment) shader.
+		/// </summary>
+		Pixel,
+
+		/// <summary>
+		/// Vertex shader.
+		/// </summary>
+		Vertex
+	}
+
+	/// <summary>
+	/// Bazowy interfejs dla shaderów.
+	/// </summary>
+	public interface IShader
+		: ResourcesManager.IResource
+	{
+		/// <summary>
+		/// Identyfikator shadera.
+		/// </summary>
+		int ShaderId { get; }
+
+		/// <summary>
+		/// Typ shadera.
+		/// </summary>
+		ShaderType Type { get; }
+
+		//Wstępnie tylko jeden, później będzie rozbudowane o więcej.
+		#region Uniform binding
+		/// <summary>
+		/// Ustawia parametr.
+		/// </summary>
+		/// <param name="name">Nazwa.</param>
+		/// <param name="matrix">Macierz.</param>
+		void Set(string name, Matrix4 matrix);
+		#endregion
+	}
+}
