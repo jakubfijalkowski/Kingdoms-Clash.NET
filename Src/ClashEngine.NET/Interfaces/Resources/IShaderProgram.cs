f998c7229b0e57d12f5948bcb7e448314d4f29da
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-23 18:03:48 +0200

    Kompletnie zmieniony interfejs dla shaderów.

---
+++ Src/ClashEngine.NET/Interfaces/Resources/IShaderProgram.cs
@@ -0,0 +1,28 @@
+﻿namespace ClashEngine.NET.Interfaces.Resources
+{
+	/// <summary>
+	/// Bazowa klasa dla shaderów. 
+	/// </summary>
+	public interface IShaderProgram
+		: ResourcesManager.IResource
+	{
+		#region Properties
+		/// <summary>
+		/// Plik źródłowy z kodem fragment shadera.
+		/// </summary>
+		string FragmentShaderFile { get; }
+
+		/// <summary>
+		/// Plik źródłowy z kodem vertex shadera.
+		/// </summary>
+		string VertexShaderFile { get; }
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// Binduje shader program.
+		/// </summary>
+		void Bind();
+		#endregion
+	}
+}
