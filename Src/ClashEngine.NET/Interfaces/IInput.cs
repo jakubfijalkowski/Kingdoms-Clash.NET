7cf36aa995863f3dc75346a2c0876f2891fb8a86
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-29 12:18:17 +0200

    Dodanie interfejsu dla wejścia.

---
+++ Src/ClashEngine.NET/Interfaces/IInput.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Input;
+
+namespace ClashEngine.NET.Interfaces
+{
+	/// <summary>
+	/// Bazowy interfejs dla wejścia.
+	/// </summary>
+	public interface IInput
+	{
+		#region Properties
+		/// <summary>
+		/// Klawiatura.
+		/// </summary>
+		KeyboardDevice Keyboard { get; }
+
+		/// <summary>
+		/// Mysz.
+		/// </summary>
+		MouseDevice Mouse { get; }
+		#endregion
+	}
+}
