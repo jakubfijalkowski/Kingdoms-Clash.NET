4285630174cb98d96e2785e57353753c09bb6612
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-29 19:36:18 +0200

    Zalążek pierwszej prostej gry-testu opartej na ClashEngine.NET.

---
+++ Src/Kingdoms Clash.NET/Player.cs
@@ -0,0 +1,30 @@
+﻿using ClashEngine.NET.Components;
+using ClashEngine.NET.EntitiesManager;
+using ClashEngine.NET.Resources;
+using ClashEngine.NET.ResourcesManager;
+using ClashEngine.NET.Interfaces.EntitiesManager;
+using OpenTK;
+
+namespace Kingdoms_Clash.NET
+{
+	class Player
+		: GameEntity
+	{
+		IAttribute<Vector2> Position;
+
+		public Player()
+			: base("Player")
+		{
+
+		}
+
+		public override void InitEntity()
+		{
+			this.AddComponent(new Sprite("PlayerShip", ResourcesManager.Instance.Load<Texture>("PlayerShip.png")));
+
+			this.GetOrCreateAttribute<Vector2>("Size").Value = new Vector2(105, 64);
+			this.Position = this.GetOrCreateAttribute<Vector2>("Position");
+			this.Position.Value = new Vector2((800.0f - 105.0f) / 2, 600.0f - 64.0f);
+		}
+	}
+}
---
+++ Src/Kingdoms Clash.NET/SimpleGame.cs
@@ -0,0 +1,56 @@
+﻿using ClashEngine.NET;
+using OpenTK.Graphics.OpenGL;
+
+namespace Kingdoms_Clash.NET
+{
+	/// <summary>
+	/// Prosta gra zbudowana za pomocą ClashEngine.NET i OpenTK.
+	/// </summary>
+	class SimpleGame
+		: Game
+	{
+		Player player;
+
+		public SimpleGame()
+			: base("SimpleGame.NET", 800, 600, false)
+		{ }
+
+		public override void Init()
+		{
+			this.ResourcesManager.ContentDirectory = "Content";
+
+			GL.Enable(EnableCap.Texture2D);
+			GL.Enable(EnableCap.Blend);
+			GL.BlendFunc(BlendingFactorSrc.One, BlendingFactorDest.OneMinusSrcAlpha);
+
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.LoadIdentity();
+			GL.Ortho(0.0, this.Width, this.Height, 0.0, 0.0, 1.0);
+
+			GL.MatrixMode(MatrixMode.Modelview);
+			GL.LoadIdentity();
+
+			GL.ClearColor(0.0f, 0.3f, 0.0f, 1.0f);
+
+			player = new Player();
+			player.InitEntity();
+
+			base.Init();
+		}
+
+		public override void Render()
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+			player.Render();
+			base.Render();
+		}
+
+		static void Main(string[] args)
+		{
+			using (SimpleGame game = new SimpleGame())
+			{
+				game.Run();
+			}
+		}
+	}
+}
