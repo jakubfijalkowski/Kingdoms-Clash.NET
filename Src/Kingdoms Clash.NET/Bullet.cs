74af69ca4908d50a8ca864bc4aca64d63ff7b705
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-08-29 22:22:26 +0200

    Dodanie strzelania i zestrzeliwania przeciwników.

---
+++ Src/Kingdoms Clash.NET/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using ClashEngine.NET.Components;
+using ClashEngine.NET.EntitiesManager;
+using ClashEngine.NET.Interfaces.EntitiesManager;
+using ClashEngine.NET.Resources;
+using ClashEngine.NET.ResourcesManager;
+using OpenTK;
+
+namespace Kingdoms_Clash.NET
+{
+	class Bullet
+		: GameEntity
+	{
+		public static Vector2 Size = new Vector2(10, 32);
+
+		const double MoveSpeed = 500.0f;
+
+		public IAttribute<Vector2> Position;
+
+		public Bullet(float startPos)
+			: base("Bullet")
+		{
+			//Tworzenie atrybutu pozycju przeniesione z InitEntity do konstruktora, ponieważ muszę wiedzieć, gdzie jest statek ;)
+			this.Position = this.GetOrCreateAttribute<Vector2>("Position");
+			this.Position.Value = new Vector2(startPos, 600.0f - 64.0f - Size.Y);
+		}
+
+		public override void InitEntity()
+		{
+			this.AddComponent(new Sprite("BulletSprite", ResourcesManager.Instance.Load<Texture>("PlayerBullet.png")));
+
+			this.GetOrCreateAttribute<Vector2>("Size").Value = Size;
+		}
+
+		public override void Update(double delta)
+		{
+			this.Position.Value = new Vector2(this.Position.Value.X, this.Position.Value.Y - (float)(MoveSpeed * delta));
+			base.Update(delta);
+		}
+	}
+}
--- Src/Kingdoms Clash.NET/GameScreen.cs
+++ Src/Kingdoms Clash.NET/GameScreen.cs
@@ -18,6 +18,7 @@ namespace Kingdoms_Clash.NET
 		double TimeBetweenNewEnemies = 2.0;
 		double ElapsedTime = 2.0;
 		List<Enemy> Enemies = new List<Enemy>();
+		List<Bullet> Bullets = new List<Bullet>();
 		Player Player;
 
 		public GameScreen()
@@ -37,9 +38,25 @@ namespace Kingdoms_Clash.NET
 			}
 
 			this.CheckEnemyShips(delta);
+			this.CheckBullets();
 		}
 
-		override Ke
+		/// <summary>
+		/// Obsługa strzelania. Space - strzał.
+		/// </summary>
+		/// <param name="e"></param>
+		/// <returns></returns>
+		public override bool KeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+		{
+			if (e.Key == OpenTK.Input.Key.Space)
+			{
+				Bullet bullet = new Bullet(this.Player.Position.Value.X + Player.Size.X / 2 - Bullet.Size.X / 2);
+				this.Bullets.Add(bullet);
+				this.Entities.AddEntity(bullet);
+				return true;
+			}
+			return false;
+		}
 
 		void CheckEnemyShips(double delta)
 		{
@@ -58,8 +75,8 @@ namespace Kingdoms_Clash.NET
 			if (this.ElapsedTime >= this.TimeBetweenNewEnemies)
 			{
 				Enemy newEnemy = new Enemy();
-				this.Enemies.Add(newEnemy);
 				this.Entities.AddEntity(newEnemy);
+				this.Enemies.Add(newEnemy);
 				this.ElapsedTime -= this.TimeBetweenNewEnemies;
 				if (this.TimeBetweenNewEnemies - Difficult > 0.0)
 				{
@@ -68,11 +85,47 @@ namespace Kingdoms_Clash.NET
 			}
 		}
 
+		void CheckBullets()
+		{
+			for (int i = 0; i < this.Bullets.Count; i++)
+			{
+				Bullet bullet = this.Bullets[i];
+				if (bullet.Position.Value.Y < -Bullet.Size.Y) //Wyszedł poza ekran - usuwamy
+				{
+					this.Entities.RemoveEntity(bullet);
+					this.Bullets.RemoveAt(i);
+					--i;
+				}
+			}
+
+			//Sprawdzamy każdy-z-każdym, czy pocisk zderzył się z przeciwnikiem
+			for (int i = 0; i < this.Enemies.Count; i++)
+			{
+				Enemy enemy = this.Enemies[i];
+				for (int j = 0; j < this.Bullets.Count; j++)
+				{
+					Bullet bullet = this.Bullets[j];
+
+					if (RectangleCollision(bullet.Position.Value.X, bullet.Position.Value.X + Bullet.Size.X, bullet.Position.Value.Y, bullet.Position.Value.Y + Bullet.Size.Y,
+						enemy.Position.Value.X, enemy.Position.Value.X + Enemy.Size.X, enemy.Position.Value.Y, enemy.Position.Value.Y + Enemy.Size.Y))
+					{
+						//Kolizja!
+						this.Entities.RemoveEntity(enemy);
+						this.Entities.RemoveEntity(bullet);
+						this.Enemies.RemoveAt(i);
+						this.Bullets.RemoveAt(j);
+						--i;
+						--j;
+					}
+				}
+			}
+		}
+
 		bool CheckCollisions()
 		{
 			foreach (var enemy in this.Enemies)
 			{
-				if (enemy.Position.Value.Y >= this.Player.Position.Value.Y) //Pseudo-kolizja prostokąt-prostokąt
+				if (enemy.Position.Value.Y + Enemy.Size.Y >= this.Player.Position.Value.Y) //Pseudo-kolizja prostokąt-prostokąt
 				{
 					//Dla ułatwienia sprawdzam tylko czy enemy.Y >= player.Y i czy któryś z dolnych wierzchołków przeciwnika jest pomiędzy naszym statkiem
 					return (enemy.Position.Value.X >= this.Player.Position.Value.X && enemy.Position.Value.X <= this.Player.Position.Value.X + Player.Size.X)
@@ -80,6 +133,14 @@ namespace Kingdoms_Clash.NET
 				}
 			}
 			return false;
+		}
+
+		private static bool RectangleCollision(float l1, float r1, float t1, float b1,
+			float l2, float r2, float t2, float b2)
+		{
+			if (b1 < t2 || t1 > b2 || r1 < l2 || l1 > r2)
+				return false;
+			return true;
 		}
 	}
 }
