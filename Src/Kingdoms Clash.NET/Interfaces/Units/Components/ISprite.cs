71b434b6521bb2e915965c3334401e941aa23545
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-10-02 17:09:34 +0200

    Zmiana nazy komponentu IStaticImage na ISprite. Implementacja tego komponentu.

---
+++ Src/Kingdoms Clash.NET/Interfaces/Units/Components/ISprite.cs
@@ -0,0 +1,9 @@
+﻿namespace Kingdoms_Clash.NET.Interfaces.Units.Components
+{
+	/// <summary>
+	/// Jednostka posiada obrazek(sprite).
+	/// </summary>
+	public interface ISprite
+		: ClashEngine.NET.Interfaces.Components.ISprite, IUnitComponent
+	{ }
+}
---
+++ Src/Kingdoms Clash.NET/Units/Components/Sprite.cs
@@ -0,0 +1,39 @@
+﻿using ClashEngine.NET.Resources;
+using ClashEngine.NET.ResourcesManager;
+using OpenTK;
+
+namespace Kingdoms_Clash.NET.Units.Components
+{
+	using Interfaces.Units;
+	using Interfaces.Units.Components;
+
+	/// <summary>
+	/// Jednostka posiada obrazek(sprite).
+	/// </summary>
+	public class Sprite
+		: ClashEngine.NET.Components.Sprite, ISprite
+	{
+		public Sprite()
+			: base("Sprite")
+		{ }
+
+		public override void Init(ClashEngine.NET.Interfaces.EntitiesManager.IGameEntity owner)
+		{
+			base.Init(owner);
+
+			string path = (this.Owner as IUnitDescription).Attributes.Get("Image") as string;
+
+			base.Init(ResourcesManager.Instance.Load<Texture>(path));
+			this.Owner.Attributes.GetOrCreate<Vector2>("Size").Value = new Vector2(
+				(this.Owner as IUnitDescription).Width,
+				(this.Owner as IUnitDescription).Height);
+		}
+
+		#region ICloneable Members
+		public object Clone()
+		{
+			return new Sprite();
+		}
+		#endregion
+	}
+}
