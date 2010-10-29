b7b181293e76ce1e6b53cf29ca0de62e3907548b
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-10-30 00:00:05 +0200

    Dodanie dwóch interfejsów wiążących silnik z GUI.

---
+++ Src/ClashEngine.NET/Interfaces/Gui/IGuiEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ClashEngine.NET.Interfaces.Gui
+{
+	/// <summary>
+	/// Kontener na kontrolki jako encja gry.
+	/// </summary>
+	public interface IGuiEntity
+		: IGuiContainer, EntitiesManager.IGameEntity
+	{ }
+}
---
+++ Src/ClashEngine.NET/Interfaces/Gui/IGuiScreen.cs
@@ -0,0 +1,10 @@
+﻿namespace ClashEngine.NET.Interfaces.Gui
+{
+	/// <summary>
+	/// Ekran jako kontener na kontrolki.
+	/// </summary>
+	public interface IGuiScreen
+		: IGuiContainer, ScreensManager.IScreen
+	{
+	}
+}
