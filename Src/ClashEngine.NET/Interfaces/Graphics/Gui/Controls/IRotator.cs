5f00e26aacbb93c735d00ece0bf23bc169c77aa4
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2010-11-30 20:01:11 +0100

    Dodanie interfejsów dla kontrolki rotatora.

---
+++ Src/ClashEngine.NET/Interfaces/Graphics/Gui/Controls/IRotator.cs
@@ -0,0 +1,36 @@
+﻿namespace ClashEngine.NET.Interfaces.Graphics.Gui.Controls
+{
+	/// <summary>
+	/// Kontrolka - rotator.
+	/// </summary>
+	public interface IRotator
+		: IControl
+	{
+		/// <summary>
+		/// Obiekty rotatora.
+		/// </summary>
+		IRotatorObjectsCollection Objects { get; }
+
+		/// <summary>
+		/// Liczba elementów które mogą być aktualnie wyświetlane.
+		/// </summary>
+		uint SelectedItemsCount { get; set; }
+
+		/// <summary>
+		/// Rozmiar pojedynczej strony.
+		/// </summary>
+		uint PageSize { get; set; }
+
+		/// <summary>
+		/// Aktualna strona.
+		/// </summary>
+		uint CurrentPage { get; set; }
+
+		/// <summary>
+		/// Pobiera jeden z wybranych elementów.
+		/// </summary>
+		/// <param name="index">Indeks. Od 0 do SelectedItemsCount.</param>
+		/// <returns></returns>
+		object this[int index] { get; }
+	}
+}
---
+++ Src/ClashEngine.NET/Interfaces/Graphics/Gui/Controls/IRotatorObjectsCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClashEngine.NET.Interfaces.Graphics.Gui
+{
+	/// <summary>
+	/// Interfejs dla obiektów rotatora.
+	/// Wymaga, by wszystkie obiekty w kolekcji były tego samego typu.
+	/// </summary>
+	public interface IRotatorObjectsCollection
+		: ICollection<object>
+	{
+		/// <summary>
+		/// Typ obiektów w kolekcji.
+		/// </summary>
+		Type ObjectsType { get; }
+
+		/// <summary>
+		/// Pobiera obiekt na wskazanej pozycji.
+		/// </summary>
+		/// <param name="index">Indeks.</param>
+		/// <returns></returns>
+		object this[int index] { get; }
+	}
+}
