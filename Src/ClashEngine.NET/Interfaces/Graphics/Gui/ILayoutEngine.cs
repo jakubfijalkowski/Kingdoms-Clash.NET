1e7478f0c38bb31c7db683876e7c9ed78c998c4d
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2011-01-08 00:18:36 +0100

    Dodanie ILayoutEngine.

---
+++ Src/ClashEngine.NET/Interfaces/Graphics/Gui/ILayoutEngine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ClashEngine.NET.Interfaces.Graphics.Gui
+{
+	/// <summary>
+	/// Silnik odpowiedzialny za układanie kontrolek/obiektów.
+	/// </summary>
+	public interface ILayoutEngine
+	{
+		/// <summary>
+		/// Lista elementów.
+		/// </summary>
+		IList<IPositionableElement> Elements { get; set; }
+
+		/// <summary>
+		/// Uaktualnia wszystkie elementy.
+		/// </summary>
+		void Layout();
+	}
+}
