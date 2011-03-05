ffe64d62d88fad2ec058c50b365bd736cd402155
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2011-03-05 13:37:57 +0100

    Dodanie implementacji IServer opartej na TCP. Dodanie wewnętrznej kolekcji implementującej IClientsCollection.

---
+++ Src/ClashEngine.NET/Net/Internals/ServerClientsCollection.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClashEngine.NET.Net.Internals
+{
+	using Interfaces.Net;
+
+	/// <summary>
+	/// Wewnętrzna kolekcja klientów.
+	/// </summary>
+	internal class ServerClientsCollection
+		: IClientsCollection
+	{
+		#region Private fields
+		private List<IClient> Clients = new List<IClient>();
+		#endregion
+
+		#region IList<IClient> Members
+		/// <summary>
+		/// Pobiera indeks wskazanego elementu.
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
+		public int IndexOf(IClient item)
+		{
+			return this.Clients.IndexOf(item);
+		}
+
+		/// <summary>
+		/// Niewspierane.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <param name="item"></param>
+		public void Insert(int index, IClient item)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Niewspierane.
+		/// </summary>
+		/// <param name="index"></param>
+		public void RemoveAt(int index)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Pobiera element ze wskazanego miejsca.
+		/// Zmiana elementu jest niewspierana.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public IClient this[int index]
+		{
+			get { return this.Clients[index]; }
+			set { throw new NotSupportedException(); }
+		}
+		#endregion
+
+		#region ICollection<IClient> Members
+		/// <summary>
+		/// Niewspierane.
+		/// </summary>
+		/// <param name="item"></param>
+		public void Add(IClient item)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Niewspierane.
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
+		public bool Remove(IClient item)
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Niewspierane.
+		/// </summary>
+		public void Clear()
+		{
+			throw new NotSupportedException();
+		}
+
+		/// <summary>
+		/// Sprawdza, czy kolekcja zawiera podaney element.
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
+		public bool Contains(IClient item)
+		{
+			return this.Clients.Contains(item);
+		}
+
+		/// <summary>
+		/// Kopiuje kolekcję do tablicy.
+		/// </summary>
+		/// <param name="array"></param>
+		/// <param name="arrayIndex"></param>
+		public void CopyTo(IClient[] array, int arrayIndex)
+		{
+			this.Clients.CopyTo(array, arrayIndex);
+		}
+
+		/// <summary>
+		/// Pobiera liczbę elementów w kolekcji.
+		/// </summary>
+		public int Count
+		{
+			get { return this.Clients.Count; }
+		}
+
+		/// <summary>
+		/// Klasy nieupoważnione nie mogą zmieniać tej kolekcji.
+		/// </summary>
+		public bool IsReadOnly
+		{
+			get { return true; }
+		}
+		#endregion
+
+		#region IEnumerable<IClient> Members
+		public IEnumerator<IClient> GetEnumerator()
+		{
+			return this.Clients.GetEnumerator();
+		}
+		#endregion
+
+		#region IEnumerable Members
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return this.Clients.GetEnumerator();
+		}
+		#endregion
+
+		#region Internals
+		internal void InternalAdd(IClient item)
+		{
+			this.Clients.Add(item);
+		}
+
+		internal bool InternalRemove(IClient item)
+		{
+			return this.Clients.Remove(item);
+		}
+		#endregion
+	}
+}
---
+++ Src/ClashEngine.NET/Net/TcpServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace ClashEngine.NET.Net
+{
+	using Interfaces.Net;
+
+	/// <summary>
+	/// Serwer oparty na połączeniach TCP.
+	/// </summary>
+	public class TcpServer
+		: IServer
+	{
+		#region Private fields
+		private Internals.ServerClientsCollection _ClientsCollection = new Internals.ServerClientsCollection();
+		#endregion
+
+		#region IServer Members
+		/// <summary>
+		/// Dane, na których serwer nasłuchuje.
+		/// </summary>
+		public IPEndPoint Endpoint { get; private set; }
+
+		/// <summary>
+		/// Maksymalna liczba klientów podłączonych do serwera.
+		/// </summary>
+		public uint MaxClients { get; private set; }
+
+		/// <summary>
+		/// Lista klientów aktualnie podłączonych do serwera.
+		/// </summary>
+		public IClientsCollection Clients
+		{
+			get { return this._ClientsCollection; }
+		}
+		#endregion
+
+		#region Constructors
+		/// <summary>
+		/// Inicjalizuje serwer.
+		/// </summary>
+		/// <param name="endpoint"><see cref="Endpoint"/></param>
+		/// <param name="maxClients"><see cref="MaxClients"/></param>
+		public TcpServer(IPEndPoint endpoint, uint maxClients)
+		{
+			this.Endpoint = endpoint;
+			this.MaxClients = maxClients;
+		}
+
+		/// <summary>
+		/// Inicjalizuje serwer.
+		/// </summary>
+		/// <param name="port">Port, na którym serwer nasłuchuje.</param>
+		/// <param name="maxClients"><see cref="MaxClients"/></param>
+		public TcpServer(int port, uint maxClients)
+			: this(new IPEndPoint(IPAddress.Any, port), maxClients)
+		{ }
+		#endregion
+	}
+}
