c20fc9c4393f166bda31f0539cb450df41e20d4e
Jakub 'Fiołek' Fijałkowski <fiolek94@gmail.com>
2011-04-08 21:23:42 +0200

    Dodanie serializera binarnego przystosowanego do silnikowej obsługi sieci. Dodanie testó dla niego

---
+++ Src/ClashEngine.NET.Tests/Utilities/NetBinarySerializerTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ClashEngine.NET.Tests.Utilities
+{
+	using NET.Utilities;
+
+	[TestFixture]
+	public class NetBinarySerializerTests
+	{
+		#region Datas
+		private static readonly object[] SimpleData = new object[] { (int)1, (short)2, null, (byte)5 };
+		private static readonly byte[] SimpleDataSerialized = new byte[]
+		{
+			0x01, 0x00, 0x00, 0x00, //1, 0-3
+			0x02, 0x00,             //2, 4-5
+			0x00,                   //3, 6
+			0x05                    //4, 7
+		};
+
+		private static readonly object[] Data = new object[] { true, null, (byte)1, 'b', DBNull.Value, (decimal)1, (double)1, (Int16)(-5), (Int32)(-6), (Int64)7,
+				(SByte)(-2), (Single)1, (UInt16)9, (UInt32)10, (UInt64)11 };
+		private static readonly byte[] DataSerialized = new byte[]
+		{	 
+			0x01,                                           //1, 0
+			0x00,                                           //2, 1
+			0x01,                                           //3, 2
+			0x62, 0x00,                                     //4, 3-4
+			0x00,                                           //5, 5
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x3F, //6, 6-13
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x3F, //7, 13-21
+			0xFB, 0xFF,                                     //8, 22-23
+			0xFA, 0xFF, 0xFF, 0xFF,                         //9, 24-27
+			0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //10, 28-35
+			0xFE,                                           //11, 36
+			0x00, 0x00, 0x80, 0x3F,                         //12, 37-40
+			0x09, 0x00,                                     //13, 41-42
+			0x0A, 0x00, 0x00, 0x00,                         //14, 43-46
+			0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  //15, 47-54
+		};
+
+		private static readonly string Text = "Zażółcić gęślą jaźń";
+		private static readonly byte[] TextSerialized;
+
+		/// <summary>
+		/// Taka customowa inicjalizacja jest o wiele prostsza ;)
+		/// </summary>
+		static NetBinarySerializerTests()
+		{
+			TextSerialized = new byte[2 + Text.Length * 2];
+			TextSerialized[0] = (byte)Text.Length;
+			TextSerialized[1] = 0x00;
+			System.Text.Encoding.Unicode.GetBytes(Text, 0, Text.Length, TextSerialized, 2);
+		}
+		#endregion
+
+		[Test]
+		public void SimpleSerializationList()
+		{
+			byte[] output = NetBinarySerializer.Serialize(SimpleData);
+			CollectionAssert.AreEqual(SimpleDataSerialized, output);
+		}
+
+		[Test]
+		public void SimpleSerialization()
+		{
+			byte[] output = new byte[SimpleDataSerialized.Length];
+			NetBinarySerializer.Serialize(output, SimpleData);
+			CollectionAssert.AreEqual(SimpleDataSerialized, output);
+		}
+
+		[Test]
+		public void SerializationList()
+		{
+			byte[] output = NetBinarySerializer.Serialize(Data);
+			CollectionAssert.AreEqual(DataSerialized, output);
+		}
+
+		[Test]
+		public void Serialization()
+		{
+			byte[] output = new byte[DataSerialized.Length];
+			NetBinarySerializer.Serialize(output, Data);
+			CollectionAssert.AreEqual(DataSerialized, output);
+		}
+
+		[Test]
+		public void StringSerializationList()
+		{
+			byte[] output = NetBinarySerializer.Serialize(Text);
+			CollectionAssert.AreEqual(TextSerialized, output);
+		}
+
+		[Test]
+		public void StringSerialization()
+		{
+			byte[] output = new byte[TextSerialized.Length];
+			NetBinarySerializer.Serialize(output, Text);
+			CollectionAssert.AreEqual(TextSerialized, output);
+		}
+	}
+}
---
+++ Src/ClashEngine.NET/Utilities/NetBinarySerializer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClashEngine.NET.Utilities
+{
+	/// <summary>
+	/// Helper przy serializowaniu danych binarnych.
+	/// </summary>
+	public static class NetBinarySerializer
+	{
+		public static byte[] Serialize(params object[] objs)
+		{
+			List<byte> outputList = new List<byte>();
+			foreach (var obj in objs)
+			{
+				if (obj == null)
+					outputList.Add(0);
+				else if(obj is IConvertible)
+				{
+					switch ((obj as IConvertible).GetTypeCode())
+					{
+						case TypeCode.Boolean:
+							outputList.Add(((bool)obj ? (byte)1 : (byte)0));
+							break;
+						case TypeCode.Byte:
+							outputList.Add((byte)obj);
+							break;
+						case TypeCode.Char:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((char)obj)));
+							break;
+						case TypeCode.DBNull:
+							outputList.Add(0);
+							break;
+						case TypeCode.Decimal:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((double)(decimal)obj)));
+							break;
+						case TypeCode.Double:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((double)obj)));
+							break;
+						case TypeCode.Int16:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((Int16)obj)));
+							break;
+						case TypeCode.Int32:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((Int32)obj)));
+							break;
+						case TypeCode.Int64:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((Int64)obj)));
+							break;
+						case TypeCode.SByte:
+							outputList.Add((byte)(sbyte)obj);
+							break;
+						case TypeCode.Single:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((Single)obj)));
+							break;
+						case TypeCode.UInt16:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((UInt16)obj)));
+							break;
+						case TypeCode.UInt32:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((UInt32)obj)));
+							break;
+						case TypeCode.UInt64:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((UInt64)obj)));
+							break;
+						case TypeCode.String:
+							outputList.AddRange(GetLittleEndian(BitConverter.GetBytes((UInt16)((string)obj).Length)));
+							outputList.AddRange(Encoding.Unicode.GetBytes((string)obj));
+							break;
+					}
+				}
+			}
+			return outputList.ToArray();
+		}
+
+		public static void Serialize(byte[] output, params object[] objs)
+		{
+			for (int i = 0, j = 0; i < objs.Length; i++)
+			{
+				if (objs[i] == null)
+					output[j++] = 0;
+				else if (objs[i] is IConvertible)
+				{
+					switch ((objs[i] as IConvertible).GetTypeCode())
+					{
+						case TypeCode.Boolean:
+							output[j++] = (bool)objs[i] ? (byte)1 : (byte)0;
+							break;
+						case TypeCode.Byte:
+							output[j++] = (byte)objs[i];
+							break;
+						case TypeCode.Char:
+							GetAndCopy(BitConverter.GetBytes((char)objs[i]), output, j);
+							j += 2;
+							break;
+						case TypeCode.DBNull:
+							output[j++] = 0;
+							break;
+						case TypeCode.Decimal:
+							GetAndCopy(BitConverter.GetBytes((double)(decimal)objs[i]), output, j);
+							j += 8;
+							break;
+						case TypeCode.Double:
+							GetAndCopy(BitConverter.GetBytes((double)objs[i]), output, j);
+							j += 8;
+							break;
+						case TypeCode.Int16:
+							GetAndCopy(BitConverter.GetBytes((Int16)objs[i]), output, j);
+							j += 2;
+							break;
+						case TypeCode.Int32:
+							GetAndCopy(BitConverter.GetBytes((Int32)objs[i]), output, j);
+							j += 4;
+							break;
+						case TypeCode.Int64:
+							GetAndCopy(BitConverter.GetBytes((Int64)objs[i]), output, j);
+							j += 8;
+							break;
+						case TypeCode.SByte:
+							output[j++] = (byte)(sbyte)objs[i];
+							break;
+						case TypeCode.Single:
+							GetAndCopy(BitConverter.GetBytes((Single)objs[i]), output, j);
+							j += 4;
+							break;
+						case TypeCode.UInt16:
+							GetAndCopy(BitConverter.GetBytes((UInt16)objs[i]), output, j);
+							j += 2;
+							break;
+						case TypeCode.UInt32:
+							GetAndCopy(BitConverter.GetBytes((UInt32)objs[i]), output, j);
+							j += 4;
+							break;
+						case TypeCode.UInt64:
+							GetAndCopy(BitConverter.GetBytes((UInt64)objs[i]), output, j);
+							j += 8;
+							break;
+						case TypeCode.String:
+							GetAndCopy(BitConverter.GetBytes((UInt16)((string)objs[i]).Length), output, j);
+							j += 2;
+							Array.Copy(Encoding.Unicode.GetBytes((string)objs[i]), 0, output, j, ((string)objs[i]).Length * 2);
+							j += ((string)objs[i]).Length;
+							break;
+					}
+				}
+			}
+		}
+
+		private static byte[] GetLittleEndian(byte[] input)
+		{
+			if (BitConverter.IsLittleEndian)
+				return input;
+			Array.Reverse(input);
+			return input;
+		}
+
+		private static void GetAndCopy(byte[] input, byte[] array, int i)
+		{
+			Array.Copy(GetLittleEndian(input), 0, array, i, input.Length);
+		}
+	}
+}
